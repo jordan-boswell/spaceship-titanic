88d701abdafa8f33acc891e1d993e1063d42c610
Thuy Linh Nguyen <92179640+Thuyn8@users.noreply.github.com>
2022-11-11 20:13:10 -0800

    stacking 1

--- analysis/stacking 1.R
+++ analysis/stacking 1.R
@@ -1,3 +1,4 @@
+#load data
 # Thuy: '~/Documents/GitHub/spaceship-titanic'
 # Jordan: 'C:/Users/jbos1/Desktop/Projects/Kaggle/spaceship-titanic'
 # Jordan Laptop: 'C:/Users/User/Documents/Projects/Kaggle/spaceship-titanic'
@@ -11,33 +12,24 @@ doParallel::registerDoParallel()
 source('analysis/0 Functions.R')
 
 # read in data
-ship <- read.csv('data/ship.csv')
-ship_imp_res <- read.csv('data/ship_imputed_with_response.csv')
 ship_imp_nores <- read.csv('data/ship_imputed_no_response.csv')
 
-# change column types to numeric and factor
-ship <- setColTypesForModeling(ship)
-ship_imp_res <- setColTypesForModeling(ship_imp_res)
+#format the data
 ship_imp_nores <- setColTypesForModeling(ship_imp_nores)
 
+#get the number of the cols of the model matrix
 cols <- c('Age', 'CabinSize', 'CryoSleep', 'Deck', 'Destination', 'GID', 'GroupSize', 'HasSpent', 'HomePlanet', 'IID', 'Num', 'RoomService', 'ShoppingMall', 'Side', 'Spa', 'Spending', 'Transported', 'VIP', 'VRDeck')
-
 num_cols <- numDesignMatColsFromDataset(ship[, cols])
 
-split_indices <-
-  list(
-    analysis = which(ship$Train == 'TRUE'),
-    assessment = which(ship$Train == 'FALSE')
-  )
-splits <- make_splits(split_indices, ship[, cols])
-ship_train <- training(splits)
-ship_test <- testing(splits)
-folds <- vfold_cv(ship_train)
+# create the recipe
+rec <- recipe(Transported ~ ., data = ship_imp_nores[, cols]) %>%
+  step_dummy(all_nominal_predictors())
 
-rec <- recipe(Transported ~ ., data = ship_train) %>%
-  step_dummy(all_nominal_predictors()) %>%
-  step_unknown()
+#create the folds
+folds <- vfold_cv(ship_imp_nores[, cols])
 
+#base models
+## xgboost
 xg_spec <- boost_tree(
   mtry = tune(),
   trees = tune(),
@@ -63,7 +55,7 @@ xg_grid <- grid_latin_hypercube(
   learn_rate(),
   loss_reduction(),
   sample_size = sample_prop(),
-  size = 3
+  size = 30
 )
 
 set.seed(1)
@@ -72,18 +64,10 @@ xg_res <- xg_wf %>%
             grid = xg_grid,
             control = control_grid(save_pred = T))
 
-xg_final_wf <- xg_wf %>%
-  finalize_workflow(select_best(xg_res, 'accuracy'))
+## Lasso
+ls_spec <- logistic_reg(mode = "classification",
+                        penalty = tune(),
+                        mixture = 1,
+                        set_engine = "glmnet")
 
-xg_final_fit <- xg_final_wf %>% fit(ship_train)
-
-predictions <- collect_predictions(xg_final_fit)$Transported
-write.csv(
-  data.frame(
-    PassengerId = ship$PassengerId[ship$Train == 'FALSE'],
-    Transported = ifelse(predictions == 'TRUE', 'True', 'False')
-  ),
-  file = 'submissions/basic_xg.csv',
-  quote = F,
-  row.names = F
-)
+#meta model
