14647cc1beca4c370df294226ae8ddfa8773b51f
Jordan <jboswell30@gatech.edu>
2022-11-11 22:15:00 -0800

    Update stacking 1.R

--- analysis/stacking 1.R
+++ analysis/stacking 1.R
@@ -1,35 +1,59 @@
-#load data
 # Thuy: '~/Documents/GitHub/spaceship-titanic'
 # Jordan: 'C:/Users/jbos1/Desktop/Projects/Kaggle/spaceship-titanic'
 # Jordan Laptop: 'C:/Users/User/Documents/Projects/Kaggle/spaceship-titanic'
-setwd('~/Documents/GitHub/spaceship-titanic')
+setwd('C:/Users/User/Documents/Projects/Kaggle/spaceship-titanic')
 
 library(tidymodels)
 library(xgboost)
+library(randomForest)
 
 library(doParallel)
 doParallel::registerDoParallel()
 source('analysis/0 Functions.R')
 
-# read in data
+# Load the Data
 ship_imp_nores <- read.csv('data/ship_imputed_no_response.csv')
 
-#format the data
+# Format the Data
 ship_imp_nores <- setColTypesForModeling(ship_imp_nores)
+train <- ship_imp_nores[ship_imp_nores$Train == 'TRUE',]
+test <- ship_imp_nores[ship_imp_nores$Train == 'FALSE',]
 
-#get the number of the cols of the model matrix
-cols <- c('Age', 'CabinSize', 'CryoSleep', 'Deck', 'Destination', 'GID', 'GroupSize', 'HasSpent', 'HomePlanet', 'IID', 'Num', 'RoomService', 'ShoppingMall', 'Side', 'Spa', 'Spending', 'Transported', 'VIP', 'VRDeck')
-num_cols <- numDesignMatColsFromDataset(ship[, cols])
+# Get the Columns and Calculate the Number of Columns in the Model Matrix
+cols <-
+  c(
+    'Age',
+    'CabinSize',
+    'CryoSleep',
+    'Deck',
+    'Destination',
+    'GID',
+    'GroupSize',
+    'HasSpent',
+    'HomePlanet',
+    'IID',
+    'Num',
+    'RoomService',
+    'ShoppingMall',
+    'Side',
+    'Spa',
+    'Spending',
+    'Transported',
+    'VIP',
+    'VRDeck'
+  )
+num_cols <- numDesignMatColsFromDataset(train[, cols])
 
-# create the recipe
-rec <- recipe(Transported ~ ., data = ship_imp_nores[, cols]) %>%
+# Create the Recipe
+rec <- recipe(Transported ~ ., data = train[, cols]) %>%
   step_dummy(all_nominal_predictors())
 
-#create the folds
-folds <- vfold_cv(ship_imp_nores[, cols])
+# Create the Folds
+set.seed(1)
+folds <- vfold_cv(train[, cols])
 
-#base models
-## xgboost
+# Base Models
+## XGBoost
 xg_spec <- boost_tree(
   mtry = tune(),
   trees = tune(),
@@ -64,22 +88,50 @@ xg_res <- xg_wf %>%
             grid = xg_grid,
             control = control_grid(save_pred = T))
 
+
+## Random Forest
+rf_spec <- rand_forest(mtry = tune(),
+                       trees = 2000,
+                       min_n = tune()) %>%
+  set_engine('randomForest') %>%
+  set_mode('classification')
+
+rf_wf <- workflow() %>%
+  add_model(xg_spec) %>%
+  add_recipe(rec)
+
+set.seed(1)
+rf_grid <-
+  grid_latin_hypercube(mtry() %>% range_set(c(1, num_cols)),
+                       trees(),
+                       min_n(),
+                       size = 30)
+
+set.seed(1)
+rf_res <- rf_wf %>%
+  tune_grid(resamples = folds,
+            grid = rf_grid,
+            control = control_grid(save_pred = T))
+
+
 ## Lasso
-ls_spec <- logistic_reg(mode = "classification",
-                        penalty = tune(),
-                        mixture = 1,
-                        set_engine = "glmnet")
+ls_spec <- logistic_reg(
+  mode = "classification",
+  penalty = tune(),
+  mixture = 1,
+  set_engine = "glmnet"
+)
 
 ls_rec <- rec %>% step_interact(all_predictors())
 
 ls_wf <- workflow() %>% add_model(ls_spec) %>% add_recipe(ls_rec)
 
-ls_grid <- grid_latin_hypercube(
-  penalty() %>% range_set(10 ^ seq(10,-2,length = 100)), size = 30) 
+ls_grid <- grid_latin_hypercube(penalty() %>% range_set(10 ^ seq(10, -2, length = 100)), size = 30)
 
 ls_res <- ls_wf %>%
   tune_grid(resamples = folds,
             grid = ls_grid,
             control = control_grid(save_pred = T))
 
-#meta model
+
+# Meta Model
